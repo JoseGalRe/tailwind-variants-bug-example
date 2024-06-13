2cfc68da3dc77b93118b5f9e0671302fa8bc3b03
Jose Adolfo Galdamez <jose@galre.dev>
2024-06-12 21:23:47 -0600

    🚧 cleanup init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  bracketSameLine: true,
+  endOfLine: 'lf',
+  printWidth: 128,
+  singleQuote: true,
+  tabWidth: 2
+};
+
+module.exports = config;
